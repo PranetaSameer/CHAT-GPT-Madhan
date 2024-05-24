83e57493b6c07c591a4d31fad0ed4201dd74b658
Madhan P <141417391+MadhanMohanReddy2301@users.noreply.github.com>
2024-05-24 11:17:56 +0530

    Update UI_st.py

--- UI_st.py
+++ UI_st.py
@@ -1,17 +1,32 @@
 import streamlit as st
-from models import get_gemini_response
+from models import get_gemini_response, get_gemini_response_image, yt_summerize
+from PIL import Image
+
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Gemini Image Demo")
+st.set_page_config(page_title="BOT GPT")
 
-st.header("Bot GPT")
-input=st.text_input("Input Prompt: ",key="input")
+st.header("Bot Application")
+input=st.text_area("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    
 submit=st.button("submit")
 
 ## If ask button is clicked
-
+yt ="https://www.youtube.com"
 if submit:
-    responses = get_gemini_response(input)
+    if yt in input:
+        response = yt_summerize(input)
+    st.subheader("Key Points from the video")
+    st.write(response)
+else:    
+    if image != "":
+        responses = get_gemini_response_image(input, image)
+    else:
+        responses = get_gemini_response(input)
     st.subheader("Responses:")
     st.write(responses)
