47948347e3413aa1fef952211c6eced785b926f7
Madhan P <141417391+MadhanMohanReddy2301@users.noreply.github.com>
2024-05-24 11:21:52 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,48 @@
 import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+import time
 
-genai.configure(api_key='AIzaSyCywxL3BTdCMWt22qmZIxpOJVECFNbr02s')
+genai.configure(api_key="AIzaSyCywxL3BTdCMWt22qmZIxpOJVECFNbr02s")
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
+model_video = genai.GenerativeModel(model_name="models/gemini-1.5-flash-latest")
 
+def yt_summerize(url):
+  try:
+    video_id = url.split("=")[1]
+    trans_text = YouTubeTranscriptApi.get_transcript(video_id=video_id)
+    
+    transcript = ""
+    for i in trans_text:
+      transcript += " "+i["text"]
+    return get_gemini_response(transcript)
+  except Exception as e:
+    raise e 
 
-input = 'tell me about yourself'
-def get_gemini_response(input, model=genai.GenerativeModel('gemini-1.5-pro-latest')):
-  response = model.generate_content(input)
+def get_gemini_response_image(input,image):
+  response = model.generate_content([input, image])
   return response.text
-print(get_gemini_response(input, model))
+
+def get_gemini_response(input):
+  response = model.generate_content([input])
+  return response.text
+
+def video_analisis(video_file_name):
+  print(f"Uploading file...")
+  video_file = genai.upload_file(path=video_file_name)
+  print(f"Completed upload: {video_file.uri}")
+  
+  while video_file.state.name == "PROCESSING":
+    print('.', end='')
+    time.sleep(10)
+    video_file = genai.get_file(video_file.name)
+
+  if video_file.state.name == "FAILED":
+    raise ValueError(video_file.state.name)
+  
+  prompt = "Describe this video."
+  print("Making LLM inference request...")
+  response = model_video.generate_content([prompt, video_file],
+                                    request_options={"timeout": 600})
+  return response.text
+
+    
