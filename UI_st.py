8910e6ae1d825cf8e8fd46e98fd0a1812127b655
Madhan P <141417391+MadhanMohanReddy2301@users.noreply.github.com>
2024-05-26 13:42:35 +0530

    included image generation

--- UI_st.py
+++ UI_st.py
@@ -1,6 +1,8 @@
 import streamlit as st
-from models import get_gemini_response, get_gemini_response_image, yt_summerize
+from models import get_gemini_response, get_gemini_response_image, yt_summerize, imagegen
 from PIL import Image
+import io
+
 
 
 ##initialize our streamlit app
@@ -18,7 +20,9 @@ with col3:
 
 input=st.text_area("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
+if uploaded_file:
+    file_extension = uploaded_file.name.split('.')[-1].lower()
+    
 image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -28,8 +32,18 @@ submit=st.button("submit")
 response = ''
 ## If ask button is clicked
 yt ="https://www.youtube.com"
+list = ["create image", "generate image", "create picture", "generate picture"]
+contains_keyword = any(keyword in input.lower() for keyword in list)
+
 if submit:
-    if yt in input:
+    if  contains_keyword:
+        st.write("Generating image...")
+        response = imagegen(input)
+        image = Image.open(io.BytesIO(response))
+        # Display the image in Streamlit
+        st.image(image, caption=input)
+        
+    elif yt in input:
         response = yt_summerize(input)
         st.subheader("Key Points from the video")
         st.write(response)
